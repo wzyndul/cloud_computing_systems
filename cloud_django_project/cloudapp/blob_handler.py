0e7f10cd00111a3c8d8e5e498eda4e35c2fb75de
mwysocka <mikafikapika@gmail.com>
2024-01-16 22:41:12 +0100

    Added file changes to log file

--- cloud_django_project/cloudapp/blob_handler.py
+++ cloud_django_project/cloudapp/blob_handler.py
@@ -1,17 +1,19 @@
 from concurrent.futures import ThreadPoolExecutor
-
 from django.conf import settings
 from azure.storage.blob import BlobServiceClient
 from django.http import HttpResponse
 from django.utils.encoding import escape_uri_path
+from cloudapp.utils import logger
 
 
 def create_blob_container(user):
     blob_service_client = BlobServiceClient.from_connection_string(settings.AZURE_STORAGE_CONNECTION_STRING)
-
     container_client = blob_service_client.get_container_client(user.username.lower())
+
     if not container_client.exists():
         container_client.create_container()
+
+    logger.info(f'New container created for user: {user.username.lower()}')
 
 
 def upload_blob(user, file):
@@ -20,6 +22,7 @@ def upload_blob(user, file):
 
     blob_client = container_client.get_blob_client(file.name)
     blob_client.upload_blob(file, overwrite=True)
+    logger.info(f'New file "{file.name}" uploaded for user: {user.username.lower()}')
 
 
 def parallel_upload_blob(user, files):
@@ -28,6 +31,8 @@ def parallel_upload_blob(user, files):
 
     for future in futures:
         future.result()
+
+    logger.info(f'{len(files)} file(s) uploaded for user: {user.username.lower()}')
 
 
 def list_blobs_with_properties(user):
@@ -50,6 +55,8 @@ def list_blobs_with_properties(user):
             'last_modified': last_modified,
             'size': size
         })
+
+    logger.info(f'List of files with properties returned for user: {user.username.lower()}')
     return files_with_properties
 
 
@@ -61,6 +68,7 @@ def change_blob_version(user, file_name, version):
     blob_client.start_copy_from_url(
         f'https://dataincloud.blob.core.windows.net/{user.username.lower()}/{file_name}?versionId={version}')
     blob_client.delete_blob(version_id=version)
+    logger.info(f'Version of file "{file_name}" changed for user: {user.username.lower()}')
 
 
 def download_blob(user, file_name):
@@ -72,4 +80,5 @@ def download_blob(user, file_name):
     response = HttpResponse(blob_data, content_type='application/octet-stream')
     response['Content-Disposition'] = f'attachment; filename="{escape_uri_path(file_name)}"'
 
+    logger.info(f'File "{file_name}" downloaded for user: {user.username.lower()}')
     return response
