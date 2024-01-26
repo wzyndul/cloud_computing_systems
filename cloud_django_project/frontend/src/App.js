576c4e1cefd54cdca619ed18f2b891b1fb09d4aa
mwysocka <mikafikapika@gmail.com>
2024-01-26 16:52:42 +0100

    Added more JSX pages

--- cloud_django_project/frontend/src/App.js
+++ cloud_django_project/frontend/src/App.js
@@ -1,28 +1,33 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Storage from "./pages/Storage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import ErrorPage from "./pages/ErrorPage";
 
 function App() {
     return (
         <div className="App">
-            <nav>
-                <ul>
-                    <li>
-                        <a href={"/"}>Home</a>
-                        <a href={"/storage"}>Storage</a>
-                    </li>
-                </ul>
-            </nav>
+            <div className={"navigation"}>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href={"/login"}>Login</a>
+                            <a href={"/register"}>Register</a>
+                            <a href={"/storage"}>Storage</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
 
-            <header className="App-header">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path={"/storage"} element={<Storage/>}/>
-                        <Route path={"/error"} element={<ErrorPage/>}/>
-                    </Routes>
-                </BrowserRouter>
-            </header>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/storage"} element={<Storage/>}/>
+                    <Route path={"/login"} element={<Login/>}/>
+                    <Route path={"/register"} element={<Register/>}/>
+                    <Route path={"/error"} element={<ErrorPage/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
